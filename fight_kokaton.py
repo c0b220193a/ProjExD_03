b13e0b4aadeacbe90d49ab339d082c3bf73c8a42
c0b22019 <c0b220193a@edu.teu.ac.jp>
2023-12-05 17:14:01 +0900

    追加機能３　ビームの方向

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -2,6 +2,7 @@ import os
 import random
 import sys
 import time
+import math
 
 import pygame as pg
 
@@ -66,6 +67,7 @@ class Bird:
         self.img = self.imgs[(+5, 0)]  # 右向きこうかとんをデフォ画像にする
         self.rct = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5,0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -93,6 +95,8 @@ class Bird:
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):  # なにもキーが押されていなくなかったら
             self.img = self.imgs[tuple(sum_mv)] 
         screen.blit(self.img, self.rct)
+        if not(sum_mv[0] == 0 and sum_mv[1] == 0): 
+            self.dire = (sum_mv[0], sum_mv[1])
 
 
 class Bomb:
@@ -135,7 +139,13 @@ class Beam:
         self.rct = self.img.get_rect()
         self.rct.centery = bird.rct.centery   # こうかとんの中心座標を取得
         self.rct.centerx = bird.rct.centerx+bird.rct.width/2
-        self.vx, self.vy = +5, 0
+        self.vx, self.vy = bird.dire  # こうかとんが向いている方向を取得
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.img = pg.transform.rotozoom(self.img, angle, 1.0)
+        self.rct = self.img.get_rect()
+        self.rct.centery = bird.rct.centery
+        self.rct.centerx = bird.rct.centerx + bird.rct.width / 2 + self.rct.width * self.vx / 5
+        self.rct.centery = bird.rct.centery + self.rct.height * self.vy / 5
 
     def update(self, screen: pg.Surface):
         """
